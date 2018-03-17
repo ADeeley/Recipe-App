041c3dc6b096dcb5b132fb22ef24f231d6a3952f
Adam Deeley <ad.deeley@gmail.com>
2018-03-17 16:28:09 +0000

    Added classes to hide and display recipe details.

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Recipe.css';
 
 class Input extends React.Component {
     constructor(props) {
@@ -41,6 +42,7 @@ class Recipe extends React.Component {
         super(props);
         this.state = {editMode: false};
         this.toggleEditMode = this.toggleEditMode.bind(this);
+        this.toggleIngr = this.toggleIngr.bind(this);
         this.handleDelete = this.handleDelete.bind(this);
     }
 
@@ -52,20 +54,31 @@ class Recipe extends React.Component {
         this.props.deleteCallback(this.props.name)
     }
 
+    toggleIngr() {
+        let ingr = this.refs['ingr'];
+        if (ingr.className === 'hidden') {
+            ingr.className = 'visible';
+        } else {
+            ingr.className = 'hidden';
+        }
+    }
+
     render() {
         let ingr = this.props.ingredients.map((el) => <li key={el}>{el}</li>)
         return (
             <div>
-                <h3>{this.props.name}</h3>
-                <ul>{ingr}</ul>
-                <Input 
-                    name={this.props.name}
-                    display={this.state.editMode}
-                    ingredients={this.props.ingredients}
-                    callback={this.props.callback}
-                />
-                <button onClick={this.toggleEditMode}>Edit</button>
-                <button onClick={this.handleDelete}>Delete</button>
+                <h3><a onClick={this.toggleIngr}>{this.props.name}</a></h3>
+                <div ref="ingr" className="hidden">
+                    <ul >{ingr}</ul>
+                    <Input 
+                        name={this.props.name}
+                        display={this.state.editMode}
+                        ingredients={this.props.ingredients}
+                        callback={this.props.callback}
+                    />
+                    <button onClick={this.toggleEditMode}>Edit</button>
+                    <button onClick={this.handleDelete}>Delete</button>
+                </div>
             </div>
         )
     }
